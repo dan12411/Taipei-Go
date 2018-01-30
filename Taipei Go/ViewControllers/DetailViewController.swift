1535eb585f6cb76715117aac7a4337a4caa7c724
Dan <dan12411@gmail.com>
2018-01-30 15:28:32 +0800

    ViewController: add MainVC & DetailVC

---
+++ Taipei Go/ViewControllers/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Taipei Go
+//
+//  Created by 洪德晟 on 30/01/2018.
+//  Copyright © 2018 Dan. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Taipei Go/ViewControllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taipei Go
+//
+//  Created by 洪德晟 on 30/01/2018.
+//  Copyright © 2018 Dan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
